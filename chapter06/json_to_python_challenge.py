78dcb4dc69df086a5e37521a1addf99c995c46c0
Luis Rivera <luis.j.rivera@accenturefederal.com>
2021-02-01 21:48:52 +0000

    learning about extracting data from json file

---
+++ chapter06/json_to_python_challenge.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# JSON is part of the Python Standard Library
+import json
+
+def main():
+    ## open the file
+    with open("challenge.json", "r") as challenge:
+        challengestring = challenge.read()
+
+    challengedecoded = json.loads(challengestring )
+    print(f"Full name: {challengedecoded[3]['name']}")
+    print(f"Eye color: {challengedecoded[3]['eyeColor']}")
+    print(f"Favorite Fruit: {challengedecoded[3]['favoriteFruit']}")
+
+if __name__ == "__main__":
+    main()
