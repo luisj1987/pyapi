e42e5e1490b0fb4e10e94ddf3bfec7a4506ff270
Luis Rivera <luis.j.rivera@accenturefederal.com>
2021-02-02 22:43:19 +0000

    Working with NASA APIs

--- chapter12/neows.py
+++ chapter12/neows.py
@@ -35,7 +35,22 @@ def main():
 
     ## display NASAs NEOW data
     print('Number of asteriods: ' + str(neodata["element_count"]))
-    print("")
+    
+    asteroid_sizes= {}
+    asteroid_names= []
+    danger_count= 0
+    #print(f"Total Asteroids in Range: {neodata['element_count']}")
+    for date in neodata["near_earth_objects"].keys():
+        for aster in neodata["near_earth_objects"][date]:
+            asteroid_sizes[aster["name"]]= aster["estimated_diameter"]["meters"]["estimated_diameter_max"]
+            asteroid_names.append(aster["name"])
+            if aster["is_potentially_hazardous_asteroid"]:
+                danger_count += 1
+
+    print(f"Largest Asteroid (meters): {max(asteroid_sizes)}")
+    print(f"Number of potentially hazardous asteroids: {danger_count}")
+    #input("Press ENTER to see a list of all asteroids.")
+    #print(asteroid_names) 
 
 if __name__ == "__main__":
     main()
