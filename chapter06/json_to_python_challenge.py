651c2f329e39660bc9be3bb8dd13e1430e5ae572
Luis Rivera <luis.j.rivera@accenturefederal.com>
2021-02-02 15:42:15 +0000

    working with for loops to extract data from dictionaries

--- chapter06/json_to_python_challenge.py
+++ chapter06/json_to_python_challenge.py
@@ -9,10 +9,17 @@ def main():
         challengestring = challenge.read()
 
     challengedecoded = json.loads(challengestring )
+    
+    #print(f"Full name: {challengedecoded[3]['name']}")
+    #print(f"Eye color: {challengedecoded[3]['eyeColor']}")
+    #print(f"Favorite Fruit: {challengedecoded[3]['favoriteFruit']}")
 
-    print(f"Full name: {challengedecoded[3]['name']}")
-    print(f"Eye color: {challengedecoded[3]['eyeColor']}")
-    print(f"Favorite Fruit: {challengedecoded[3]['favoriteFruit']}")
+    for info in challengedecoded:
+        print(f"Name: {info['name']}")
+        print(f"Email: {info['email']}")
+        print(f"Phone: {info['phone']}")
+        print(f"Address: {info['address']}")
+        print ("")
 
 if __name__ == "__main__":
     main()
