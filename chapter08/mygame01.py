9f9e2fd5594ba78e16b78cb33166e827ab1389bb
Luis Rivera <luis.j.rivera@accenturefederal.com>
2021-02-02 19:13:30 +0000

    gamefying directories

--- chapter08/mygame01.py
+++ chapter08/mygame01.py
@@ -20,8 +20,9 @@ def showStatus():
   print('Inventory : ' + str(inventory))
   #print an item if there is one
   if "item" in rooms[currentRoom]:
-    print('You see a ' + rooms[currentRoom]['item'])
+    print('You see a ' + str(rooms[currentRoom]['item']))
   print("---------------------------")
+  print ('You can navigate in the following direction ' + str(directions[currentRoom]))
 
 #an inventory, which is initially empty
 inventory = []
@@ -30,9 +31,10 @@ inventory = []
 ## A dictionary linking a room to other rooms
 rooms = {
 
-            'Hall' : { 
+            'Hall' : {
                   'south' : 'Kitchen',
                   'east'  : 'Dining Room',
+                  'north' : 'Bathroom',
                   'item'  : 'key'
                 },
 
@@ -43,12 +45,31 @@ rooms = {
             'Dining Room' : {
                   'west' : 'Hall',
                   'south': 'Garden',
-                  'item' : 'potion'
+                  'item' : 'potion',
+                  'north' : 'Pantry',
                },
             'Garden' : {
                   'north' : 'Dining Room'
+               },
+            'Pantry' : {
+                  'south' : 'Dining Room',
+                  'item' : ['cookie','drink']
+            },
+            'Bathroom' : {
+                'south' : 'Kitchen',
+                'east' : 'Dining Room',
+                'item' : 'sword',
             }
          }
+
+directions = {
+                'Hall' : ['South','East'],
+                'Kitchen' : ['North'],
+                'Dining Room' : ['West','South','North'],
+                'Garden' : ['North'],
+                'Pantry' : ['South'],
+                'Bathroom' : ['South','East'],
+             }
 
 #start the player in the Hall
 currentRoom = 'Hall'
@@ -65,9 +86,9 @@ while True:
   #eg typing 'go east' would give the list:
   #['go','east']
   move = ''
-  while move == '':  
+  while move == '':
     move = input('>')
-  
+
   # split allows an items to have a space on them
   # get golden key is returned ["get", "golden key"]          
   move = move.lower().split(" ", 1)
@@ -91,19 +112,22 @@ while True:
       #display a helpful message
       print(move[1] + ' got!')
       #delete the item from the room
-      del rooms[currentRoom]['item']
+      gettype = type (rooms[currentRoom]['item'])
+      if isinstance(rooms[currentRoom]['item'],list):
+        rooms[currentRoom]['item'].remove(move[1])
+      else: 
+        del rooms[currentRoom]['item']
     #otherwise, if the item isn't there to get
     else:
       #tell them they can't get it
       print('Can\'t get ' + move[1] + '!')
-
-## Define how a player can win
+      
+  ## Define how a player can win
   if currentRoom == 'Garden' and 'key' in inventory and 'potion' in inventory:
     print('You escaped the house with the ultra rare key and magic potion... YOU WIN!')
     break
 
-## If a player enters a room with a monster
-  if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item']:
+  ## If a player enters a room with a monster
+  elif 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item']:
     print('A monster has got you... GAME OVER!')
     break
-
